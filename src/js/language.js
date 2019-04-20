b583d99c9473f34e922f38a7b291808c374847d9
justforuse <allenforuse@gmail.com>
2019-04-20 10:34:01 +0800

    :art: format code

--- src/js/language.js
+++ src/js/language.js
@@ -1,17 +1,15 @@
 (function($){
+  // language buttons
   const enBtn = $('#en-version')
   const cnBtn = $('#cn-version')
-  // const closeIcon = $('#reward-close-btn')
   // go to english version
   enBtn.on('click', function(){
     const currentUrl = window.location.href
-    console.log(currentUrl)
     window.location.href = currentUrl.replace('zh-cn', 'en-us')
   })
   // go to chinese version
   cnBtn.on('click', function(){
     const currentUrl = window.location.href
-    console.log(currentUrl)
     window.location.href = currentUrl.replace('en-us', 'zh-cn')
   })
   
